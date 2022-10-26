c8cf3bfd65c9db0f7fd21f8a65511d8299705193
amalknr <116721372+amalknr@users.noreply.github.com>
2022-10-26 15:58:00 +0530

    modify comment

--- p2.py
+++ p2.py
@@ -3,9 +3,9 @@ die_roll = random.randint(1,6)
 #find the area of a triangle whose base and hight is given 1/2*bh
 #find the area of the triangle whose three sides are given
 #s=a+b+c/2
-#area=sq(s*s-a*s-c)
+#area=sqrt(s*s-a*s-c)
 #find the area of the equilateral triangle
-#/3/2*a*a
+#sqrt(3)/4*(a*a)
 import math;
 a=int(input("enter side : "))
 b=int(input("enter side : "))
